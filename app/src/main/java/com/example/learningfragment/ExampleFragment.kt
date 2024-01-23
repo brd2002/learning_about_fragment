1a1621d4dd6f3e9b583c3ba131dd7b37aa710c3b
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-23 13:57:13 +0530

    create a fragment

---
+++ app/src/main/java/com/example/learningfragment/ExampleFragment.kt
@@ -0,0 +1,9 @@
+package com.example.learningfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class ExampleFragment : Fragment(R.layout.activity_example_fragment) {
+
+}
